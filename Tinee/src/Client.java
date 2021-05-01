656814ed97d2302f159f69545b590ebd21c2ba88
HarryDavisgithub <harry-davis2011@live.co.uk>
2021-05-01 10:02:27 +0100

    Refactored splitting word code

--- Tinee/src/Client.java
+++ Tinee/src/Client.java
@@ -153,8 +153,8 @@ public class Client {
         throw new IOException("Input stream closed while reading.");
       }
       // Trim leading/trailing white space, and split words according to spaces
-      //List<String> split = Arrays.stream(raw.trim().split("\\ ")).map(x -> x.trim()).collect(Collectors.toList());
-      List<String> split = Arrays.stream(raw.split(" ", 2)).collect(Collectors.toList());
+      List<String> split = Arrays.stream(raw.trim().split(" ", 2)).map(x -> x.trim()).collect(Collectors.toList());
+      //List<String> split = Arrays.stream(raw.split(" ", 2)).collect(Collectors.toList());
       
       String cmd = split.remove(0);  // First word is the command keyword
       String[] rawArgs = split.toArray(new String[split.size()]);
